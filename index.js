f0a45e6fb01c2b6a4ed19e9d74e3ff4568957df6
Ibrahim-Abdullahi-Borle <ibroabdullahi254@gmail.com>
2025-05-19 02:18:43 -0700

    loop-lesson-1

---
+++ index.js
@@ -0,0 +1,25 @@
+// for loop
+
+for (let i = 0; i <= 200; i++) {
+  console.log("noor", i);
+}
+
+for (let i = 1; i <= 100; i++) {
+  console.log("i love you ❤", i);
+}
+// while loop
+let i = 5;
+
+while (i <= 15) {
+  console.log("heelo", i);
+  i++;
+}
+
+// do while loop
+
+let a = 10;
+
+do {
+  console.log("ahmed", a);
+  i++;
+} while (a <= 30);
